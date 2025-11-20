ece5200d268aefea5a67f0f03cf02ed30b7f4e19
Dwiki13 <112374681+Dwiki13@users.noreply.github.com>
2025-11-20 17:24:54 +0700

    relayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}>
         <ThemeProvider attribute="class" defaultTheme="system">
             {showSidebar ? (
-              <SidebarProvider defaultOpen={true}>
+              <SidebarProvider defaultOpen={false}>
                 <AppSidebar />
                 <main className="flex-1 p-6 md:p-10 relative">
                   <SidebarTrigger />
